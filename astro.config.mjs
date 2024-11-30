3ef80c0350f69b2e92f927f3af55d0215dc4fcbd
Reid Tull <reid.a.tull@gmail.com>
2024-11-30 04:17:43 -0500

    stupid scroll arrow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   output: "hybrid",
   adapter: vercel(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,25 @@
+import { apply } from "node_modules/astro/dist/core/polyfill";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       animation: {
-        fade: "fadeIn 1s ease-in-out",
+        fadeIn: "fadeInK 1s ease-in-out",
+        fadeOut: "fadeOutK 1s ease-in-out",
       },
 
       // that is actual animation
       keyframes: () => ({
-        fadeIn: {
+        fadeInK: {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
+        },
+
+        fadeOutK: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
         },
       }),
     },
